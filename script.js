18bb4108b635fb0dff158f79dfb3d7e9a66a87f5
nvthane <nvthane.personal@gmail.com>
2022-10-13 18:15:37 -0500

    Fixing attempt

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var frameNumber = 0, // start video at frame 0
     // lower numbers = faster playback
-    playbackConst = 899, 
+    playbackConst = 250, 
     // get page height from video duration
     setHeight = document.getElementById("set-height"), 
     // select video element         
